9c89c15d754ec499fe4ba4dad57fb59216a990cf
Ahmed maher <ahmed@123her.com>
2026-06-23 23:06:46 -0700

    update login page

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,49 +1,67 @@
 import api from "./axios";
 
+
 export const loginApi = async (
-email,
-password
+  email,
+  password
 ) => {
 
-const res = await api.get(
-`/users?email=${email}&password=${password}`
-);
+  const res = await api.get(
+    `/users?email=${email}`
+  );
 
-if (res.data.length === 0) {
 
-```
-throw new Error(
-  "Invalid email or password"
-);
-```
+  if (res.data.length === 0) {
 
-}
+    throw new Error(
+      "Invalid email or password"
+    );
 
-return res.data[0];
+  }
+
+
+  const user = res.data[0];
+
+
+  if (user.password !== password) {
+
+    throw new Error(
+      "Invalid email or password"
+    );
+
+  }
+
+
+  return user;
 
 };
 
+
+
+
+
 export const registerApi = async (user) => {
 
-const existingUser = await api.get(
-`/users?email=${user.email}`
-);
+  const existingUser = await api.get(
+    `/users?email=${user.email}`
+  );
 
-if (existingUser.data.length > 0) {
 
-```
-throw new Error(
-  "Email already exists"
-);
-```
+  if (existingUser.data.length > 0) {
 
-}
+    throw new Error(
+      "Email already exists"
+    );
 
-const res = await api.post(
-"/users",
-user
-);
+  }
 
-return res.data;
+
+  const res = await api.post(
+    "/users",
+    user
+  );
+
+
+  return res.data;
 
 };
